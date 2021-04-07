656970add5b79f5a0d15780f476a537a4e2d3d4a
Basil Kanaan <basil.kanaan@mail.utoronto.ca>
2021-04-07 15:59:08 +0000

    npm rm and install

--- battleroyale/src/pages/Game/index.js
+++ battleroyale/src/pages/Game/index.js
@@ -1,24 +1,24 @@
 import React, {useContext, useEffect} from "react";
 
 // styles
-import useStyles from "./styles";
+// import useStyles from "./styles";
 // components
 
 
 
 export default function Game(props) {
-    var classes = useStyles();
+    // var classes = useStyles();
 
     return ( 
         <div id="ui_play">
             <script src="jquery-3.5.1.min.js"/>
             <script type="module" language="javascript" src="controller.js"/>
 
-            <canvas id="stage" width="1000" height="1000" style="border:1px solid black;"> </canvas>
+            <canvas id="stage" width="1000" height="1000"/>
 
-            <div class="restartbtnCenter">
-                <button class="optionbtn restartbtn" id="restart">Play Again</button>
-            </div>
+            <center>
+                <button id="restart">Play Again</button>
+            </center>
         </div>   
 
     );
